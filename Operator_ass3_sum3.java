7b290cd78da9f2e75e1c9bc071742d0e54040e4b
rithikaselvaraj15 <rithikaselvaraj22@gmail.com>
2026-01-02 10:45:03 +0530

    Add files via upload

---
+++ Operator_ass3_sum3.java
@@ -0,0 +1,35 @@
+// Write a program to update a salary using operators like +=, -=, *=, /=.
+
+package com.index;
+
+public class Operator_ass3_sum3 {
+
+	public static void main(String[] args) {
+		// +=
+		
+		int salaryA=550;
+		
+	   salaryA +=1000;
+	   
+	   System.out.println(salaryA);
+	   
+	  int salaryB=1000;
+	  salaryB -= 500;
+	  
+	  System.out.println(salaryB);
+	  
+	  int salaryc=1000;
+	  salaryc *= 50;
+	  
+	  System.out.println(salaryc);
+	  
+	  
+	  int salaryd=1000;
+	  salaryd /= 50;
+	  
+	  System.out.println(salaryd);
+
+
+	}
+
+}
